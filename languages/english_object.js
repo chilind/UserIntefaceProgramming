2791bc09592bfc28546d34c2545d613243f663b6
Adib Abud Jaso <adibabud@gmail.com>
2016-03-04 13:25:23 +0100

    Remove JSON strings
    Replaced JSON strings with JS objects.

---
+++ languages/english_object.js
@@ -0,0 +1,12 @@
+var LANGUAGE_SUPPORT = {};
+LANGUAGE_SUPPORT.english = {
+  The_Flying_Dutchman:"The Flying Dutchman",
+  Login:"Login",
+  Please_login:"Please login",
+  Bar_app: "Bar app",
+  Choose_your_drink: "Choose your drink",
+  Beers: "Beers",
+  Your_drinks: "Your drinks",
+  Pay: "Pay",
+  Cancel: "Cancel"
+};
---
+++ languages/swedish_object.js
@@ -0,0 +1,11 @@
+LANGUAGE_SUPPORT.swedish = {
+  The_Flying_Dutchman:"Den flygande holländaren",
+  Login:"Logga in",
+  Please_login:"Vänligen logga in",
+  Bar_app: "Bar app",
+  Choose_your_drink: "Välj din drink",
+  Beers: "Öl",
+  Your_drinks: "Dina drycker",
+  Pay: "Betala",
+  Cancel: "Avbryt"
+};
