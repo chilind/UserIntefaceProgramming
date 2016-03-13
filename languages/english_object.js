b1855ddad283e9ad74e599d2f99837597a9b64d7
Adib Abud Jaso <adibabud@gmail.com>
2016-03-13 01:26:53 +0100

    Languages update
    Added languages to the information box

--- languages/english_object.js
+++ languages/english_object.js
@@ -1,14 +1,22 @@
 var LANGUAGE_SUPPORT = {};
 LANGUAGE_SUPPORT.english = {
-  The_Flying_Dutchman:"The Flying Dutchman",
-  Login:"Login",
-  Please_login:"Please login",
-  Bar_app: "Bar app",
-  Choose_your_drink: "Choose your drink",
-  Beers: "Beers",
-  Your_drinks: "Your drinks",
-  Pay: "Pay",
-  Cancel: "Cancel",
-  Undo: "Undo",
-  Redo: "Redo"
+    The_Flying_Dutchman:"The Flying Dutchman",
+    Login:"Login",
+    Please_login:"Please login",
+    Bar_app: "Bar app",
+    Choose_your_drink: "Choose your drink",
+    Beers: "Beers",
+    Your_drinks: "Your drinks",
+    Pay: "Pay",
+    Cancel: "Cancel",
+    Undo: "Undo",
+    Redo: "Redo",
+    Yes:"Yes",
+    No:"No",
+    Alkoholhalt:"Alcohol content",
+    Ekologisk:"Organic",
+    Type:"Type",
+    Price:"Price",
+    From:"From",
+    Producer:"Producer"
 };
